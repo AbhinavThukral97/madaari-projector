f32332affb2be46553d4725643954258b3c89468
Abhinav Thukral <thukral.abhinav@gmail.com>
2019-07-11 18:08:05 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,10 @@
+var ref = firebase.database().ref();
+
+$(document).ready(function() {
+  ref.on("value", function(data) {
+    let change = data.val().projection;
+    let avatar = change[0];
+    let action = change[1];
+    selectAction(avatar, action);
+  });
+});
